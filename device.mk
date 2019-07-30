2d1d4b0144e3ce2ca1267853c63268aaed6872a9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-07-30 15:04:58 +0530

    A6020: Build audio@2.0 HIDL service

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
@@ -46,7 +47,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths_qrd_skuh.xml \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-	 
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
